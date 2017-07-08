39219b19baf556683db9a93afc6da499d7499f84
Chris Wolf <chriswolf@Chriss-MBP.Home>
2017-07-08 11:14:20 -0400

    Added module: color_coding/colors.py

---
+++ Python2/color_coding/example_colors.py
@@ -0,0 +1,60 @@
+#
+# colors_example.py
+# 
+# A demonstration of the module colors.py
+# 
+# Name: Chris Wolf
+# E-mail: chriswolfdesign@gmail.com
+# Version: July 8, 2017
+#
+
+from colors import *
+
+if __name__ == '__main__':
+	
+	print
+	print '-------------------------------------------------------------------'
+	print 'colors.py'
+	print
+	print 'Module attempts to color code text output to aid user in debugging'
+	print 
+	print 'Warning: Some tests may fail depending on your Os and the '
+	print '         current background of your terminal/command prompt.'
+	print '-------------------------------------------------------------------'
+	print
+
+	print '-------------------------------------------------------------------'
+	print 'Example: red()'
+	print 'Function call: red(\'This text should be red!\')'
+	print '-------------------------------------------------------------------'
+	
+	print
+	print red('This text should be red!')
+	print
+	
+	print '-------------------------------------------------------------------'
+	print 'Example: blue()'
+	print 'Function call: blue(\'This text should be blue!\')'
+	print '-------------------------------------------------------------------'
+
+	print
+	print blue('This text should be blue!')
+	print
+
+	print '-------------------------------------------------------------------'
+	print 'Example: green()'
+	print 'Function call: green(\'This text should be green!\')'
+	print '-------------------------------------------------------------------'
+
+	print 
+	print green('This text should be green!')
+	print
+
+	print '-------------------------------------------------------------------'
+	print 'Example: black()'
+	print 'Function call: black(\'This text should be black!\')'
+	print '-------------------------------------------------------------------'
+
+	print
+	print black('This text should be black!')
+	print
